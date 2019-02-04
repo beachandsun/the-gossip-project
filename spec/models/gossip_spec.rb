bc39bcd93e3a302d363a1517f163427d5decd83c
hicham <hicham.34@hotmail.fr>
2019-02-04 22:19:44 +0100

    3nd commit

--- spec/models/gossip_spec.rb
+++ spec/models/gossip_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Gossip, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@user).to be_a(User)
+      expect(@user).to be_valid
+    end
+
 end
