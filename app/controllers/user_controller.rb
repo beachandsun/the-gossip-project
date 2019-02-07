4f68156551f4d8728e96c714c1b5a642e989938c
hydref <charles.dutheil@gmail.com>
2019-02-07 23:17:22 +0100

    on commence a voir le bout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,13 +5,13 @@ def new
 end
 
 def create
-	@user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], description: params[:description], age: params[:age], city_id: 34, password: params[:password])
+	@user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], description: params[:description], age: params[:age], city_id: City.all.first.id, password: params[:password])
 	@user.save
 
 	if @user.save
 		redirect_to gossip_index_path
 	else
-		render 'new'
+		render 'new'	
 	end
 
 end
